1a58f62ee1d35da76a99ad41fd9fef7e6ff22855
matt <aegis.trellaren@gmail.com>
2019-08-21 22:30:15 -0500

    Add starter kit

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
@@ -14,8 +14,22 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	public:
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
+	void InitGame();
+	void PrintGuesses();
+	void GetRandomWord();
+	void AppendScrambledChars();
 
 	// Your declarations go below!
 	private:
-	
+	FString HiddenWord;
+	int32 WordLength;
+	FString WordLen;
+	FString GuessString;
+	int32 GuessCount;
+	int32 GuessesRemaining;
+	TArray<FString> ListOfWords;
+	int32 RandomNumber;
+	FString RandomWord;
+	TArray<TCHAR> NormalWord;
+	FString ScrambledWord;
 };
